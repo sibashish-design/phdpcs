5cdd454932e28d3f2da88cff34bdcb90ce515cce
sibashish-design <kurmimaskman124@gmail.com>
2026-06-03 00:55:32 +0530

    Add Meta Pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { CartProvider } from '@/contexts/CartContext';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import { Toaster } from 'sonner';
+import { CartProvider } from "@/contexts/CartContext";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { Toaster } from "sonner";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,7 +19,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Uttarakhand Pharma Healthcare & Ayush",
-  description: "Innovating Healthcare: Bridging Gaps through Technology, Research And Collaboration",
+  description:
+    "Innovating Healthcare: Bridging Gaps through Technology, Research And Collaboration",
 };
 
 export default function RootLayout({
@@ -27,18 +29,43 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+  suppressHydrationWarning
+  className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2002332580400954');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2002332580400954&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         <CartProvider>
           <div className="min-h-screen flex flex-col">
             <Header />
-            <main className="flex-grow">
-              {children}
-            </main>
+            <main className="flex-grow">{children}</main>
             <Footer />
           </div>
+
           <Toaster
             position="top-right"
             richColors
@@ -46,8 +73,8 @@ export default function RootLayout({
             duration={4000}
             toastOptions={{
               style: {
-                fontSize: '14px',
-                fontWeight: '500',
+                fontSize: "14px",
+                fontWeight: "500",
               },
             }}
           />
